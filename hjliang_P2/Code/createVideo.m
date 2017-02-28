55b81def0a825dc1ca4aaa84dfc3079af8be7072
huaijenliang <r81810g@gmail.com>
2017-02-27 22:36:32 -0500

    add video processing

---
+++ hjliang_P2/Code/createVideo.m
@@ -0,0 +1,30 @@
+function [ writerObj ] = createVideo( images, framesNum, framesRate, fileName )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% create the video writer with 1 fps
+writerObj = VideoWriter(strcat(fileName, '.avi'));
+writerObj.FrameRate = framesRate;
+% open the video writer
+open(writerObj);
+% write the frames to the video
+newImages = resizeImages(images);
+for i = 1:length(newImages)
+    % convert the image to a frame
+    frame = im2frame(newImages{i});
+    for f = 1:framesNum 
+        writeVideo(writerObj, frame);
+    end
+end
+% close the writer object
+close(writerObj);
+
+end
+
+function [ newImages ] = resizeImages( images )
+[nRows, nCols, ~] = cellfun(@size, images);
+newNRows = min(nRows);
+newNCols = min(nCols);
+newImages = cellfun(@(x) imresize(x, [newNRows, newNCols]), images, 'UniformOutput', false);
+end
+
---
+++ hjliang_P2/Code/readVideo.m
@@ -0,0 +1,29 @@
+function [ images ] = readVideo( fileName )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+video = VideoReader(fileName);
+cellStepSize = 100;
+images  = cell(cellStepSize, 1);
+imagesSize = cellStepSize;
+imagesIndex = 0;
+
+while hasFrame(video)
+    imagesIndex = imagesIndex + 1;
+    if imagesIndex > imagesSize
+        [images, imagesSize] = expandCell(images, cellStepSize);
+    end
+    images{imagesIndex} = readFrame(video);
+end
+
+images = images(1:imagesIndex);
+
+end
+
+function [ newImages, newSize ] = expandCell(images, addSize)
+oldSize = length(images);
+newSize = oldSize + addSize;
+newImages = cell(newSize, 1);
+newImages(1:oldSize) = images;
+end
+
